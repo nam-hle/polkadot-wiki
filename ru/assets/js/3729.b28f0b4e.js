7b1eec4905fb69dd1ebbd7b39e75f10037a25142
Nam Hoang Le <nam.hoang.le@mgm-tp.com>
2021-05-23 11:11:53 +0700

    Deploy website - based on 9d688eb418b180ea88776782211ec7cd4307fd9d

---
+++ ru/assets/js/3729.b28f0b4e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[3729],{43729:(e,s,k)=>{"use strict";k.r(s)}}]);
---
+++ ru/assets/js/9127.fd6e9e81.js
@@ -0,0 +1,1 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[9127],{89127:(e,s,k)=>{"use strict";k.r(s)}}]);
